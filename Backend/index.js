0a1730046c0e549ec12ab3afc25d31c29c31f0c6
[Mohd Yousuf] <[yousuf.msoodi@gmail.com]>
2024-09-20 14:49:46 +0530

    added env file

--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,23 @@
 /* eslint-disable no-undef */
 var cors = require('cors');
+require('dotenv').config({ path: '.env' });
 const express = require("express");
 const propRoute = require("./routes/propRoute");
 const mongoose = require("mongoose");
-// const propModel = require('./models/propModel');
 
 const app = express();
 app.listen(3000);
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
-const DB_URL = "mongodb://localhost:27017/WaqfTask";
-
 mongoose
-    .connect(DB_URL)
+    .connect(process.env.DB_URL)
     .then(() => {
         console.log("connected to MongoDB");
     })
     .catch((err) => {
         console.log(err);
     });
-
-// app.get("/addProp", async (req, res) => {
-//     res.json({ name: "adminland", location: "1-1-1,admin-admin", type: "land", notes: "adminadmin" });
-//     let data = JSON.parse(JSON.stringify(req.body));
-//     console.log(req.body);
-//     console.log(data.name);
-//     // let user1 = await new Users({
-//     //     name: data.userName, email: data.email, password: data.password, isActive: false, role: 1
-//     // });
-//     // user1.save();
-// });
 
 
 app.use("/", propRoute);
